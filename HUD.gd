79b7fffb0dac11a6159c37f3c2853f2a621987f1
Fedor Indutny <fedor@indutny.com>
2020-05-24 09:52:25 -0700

    save

--- HUD.gd
+++ HUD.gd
@@ -5,3 +5,7 @@ func set_acceleration(value):
 
 func set_speed(value):
 	$Speed.text = str(value)
+
+
+func _on_Player_target_velocity_changed(new_value):
+	$Acceleration.text = str(new_value)
--- Ship.gd
+++ Ship.gd
@@ -1,15 +1,19 @@
 extends RigidBody
 
+signal target_velocity_changed
+
 # Current thrust values in local comoving frame
 var lateral_thrust = Vector2()
-var thrust = 0
 var cw_thrust = 0
 var py_thrust = Vector2()
+var target_velocity = 0
 
 export(bool) var manned = false
 
-export(float) var max_forward_velocity = 55
-export(float) var max_backward_velocity = 10
+export(float) var velocity_step = 5
+export(float) var max_forward_velocity_steps = 10
+export(float) var max_backward_velocity_steps = 4
+
 export(float) var max_lateral_velocity = 10
 export(float) var max_total_velocity = 65
 export(float) var max_cw_angular_velocity = 0.6
@@ -23,6 +27,8 @@ export(float) var max_py_torque = 0.2
 
 export(bool) var stabilization = true
 
+var max_forward_velocity = max_forward_velocity_steps * velocity_step
+var max_backward_velocity = max_backward_velocity_steps * velocity_step
 const epsilon = 1e-23
 
 func apply_stabilizing_braking(
@@ -41,18 +47,18 @@ func apply_stabilizing_braking(
 			clamp((-max_velocity - velocity) / delta, 0, max_acceleration))
 	else:
 		return acceleration
-
-func _physics_process(delta):
-	var velocity_proj = transform.basis.xform_inv(linear_velocity)
+	
+func _integrate_forces(state):
+	var velocity_proj = transform.basis.xform_inv(state.linear_velocity)
+	var delta = state.step
 	
 	# Apply stabilization to lateral/longtidual
 	var acc = Vector3(
 		lateral_thrust.x * max_lateral_acceleration,
 		lateral_thrust.y * max_lateral_acceleration,
-		-thrust * (
-			max_backward_acceleration
-			if thrust < 0 else
-			max_forward_acceleration))
+		clamp((target_velocity + velocity_proj.z) / delta,
+			max_backward_acceleration,
+			-max_forward_acceleration))
 	
 	if stabilization:
 		acc.x = apply_stabilizing_braking(
@@ -73,7 +79,8 @@ func _physics_process(delta):
 			max_backward_velocity
 				if velocity_proj.z > 0 else max_forward_velocity,
 			delta,
-			max_forward_acceleration)
+			max_backward_acceleration
+				if velocity_proj.z > 0 else max_forward_acceleration)
 	
 	# Rotational stabilization
 	var torque = Vector3(
@@ -81,7 +88,7 @@ func _physics_process(delta):
 		py_thrust.y * max_py_torque,
 		-cw_thrust * max_cw_torque)
 	if stabilization:
-		var angular_proj = transform.basis.xform_inv(angular_velocity)
+		var angular_proj = transform.basis.xform_inv(state.angular_velocity)
 		
 		# Turning must be harder at high speeds because of the speed limit
 		var py_limit = min(
@@ -111,18 +118,24 @@ func _physics_process(delta):
 	# Apply acceleration and speed limts
 	acc = transform.basis.xform(acc)
 	torque = transform.basis.xform(torque) * 2 * PI
-	linear_velocity += acc * delta
-	angular_velocity += torque * delta
-	
-	linear_velocity = min(linear_velocity.length(), max_total_velocity) * \
-		linear_velocity.normalized()
+	state.add_central_force(acc)
+	state.add_torque(torque)
 
 func _unhandled_input(event):
 	if not manned:
 		return
 	
-	thrust = Input.get_action_strength("ship_accelerate") - \
-		Input.get_action_strength("ship_break")
+	if event.is_action_pressed("ship_accelerate"):
+		target_velocity = min(
+			target_velocity + velocity_step,
+			max_forward_velocity)
+		emit_signal("target_velocity_changed", target_velocity)
+	if event.is_action_pressed("ship_break"):
+		target_velocity = max(
+			target_velocity - velocity_step,
+			-max_backward_velocity)
+		emit_signal("target_velocity_changed", target_velocity)
+	
 	lateral_thrust = Vector2(
 		Input.get_action_strength("ship_lateral_right") - \
 			Input.get_action_strength("ship_lateral_left"),
