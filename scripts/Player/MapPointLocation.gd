6dc1861d7a16b87ec7125fff4943b6cbea589f2e
Fedor Indutny <fedor@indutny.com>
2020-06-02 19:58:47 -0700

    map: fix flashes

--- scripts/Player/MapPointLocation.gd
+++ scripts/Player/MapPointLocation.gd
@@ -1,8 +1,14 @@
 extends Control
 
+var last_distance = null
+
 func set_name(name: String):
 	$NameContaintainer/VBoxContainer/Name.text = name
 
 func set_distance(distance: float):
-	$NameContaintainer/VBoxContainer/Distance.text = str(
-		round(distance * 10.0) / 10.0)
+	var truncated = round(distance * 10.0) / 10.0
+	if last_distance == truncated:
+		return
+	last_distance = truncated
+	$NameContaintainer/VBoxContainer/Distance.visible = truncated != 0
+	$NameContaintainer/VBoxContainer/Distance.text = str(truncated)
