c665b6ea97759cf3cd1d46bb4bf15029e54d1e5a
Fedor Indutny <fedor@indutny.com>
2020-06-02 22:32:54 -0700

    map-point: optimize

--- scripts/Player/MapPoint.gd
+++ scripts/Player/MapPoint.gd
@@ -2,7 +2,9 @@ extends Spatial
 
 func set_name(name: String):
 	$Viewport/Control.set_name(name)
+	$Viewport.render_target_update_mode = Viewport.UPDATE_ONCE
 
 func set_distance(distance: float):
 	$Viewport/Control.set_distance(distance)
 	$Viewport/Control.modulate.a = pow(0.5, distance)
+	$Viewport.render_target_update_mode = Viewport.UPDATE_ONCE
